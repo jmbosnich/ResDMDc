5ab272d237b97ef8245f81b892220628b7ab76c7
jonnybosnich <jonnyb@u.northwestern.edu>
2023-12-07 15:41:44 -0600

    made separate files for generating the models and observables

---
+++ basis_functions.py
@@ -0,0 +1,155 @@
+"""
+Basis functions
+
+IMPORTANT: right now these are all control systems
+TODO: give option for either controlled or unforced systems
+"""
+
+import autograd.numpy as np
+from autograd import grad
+
+class Observables(object):
+    
+    def __init__(self, choice=0, order=0, cross_terms=False):
+        self.choice = choice
+        self.order = order
+        self.cross_terms = cross_terms
+
+    def handmade(self, x, u):
+        if self.choice == 1:
+            lift_x = np.array([
+                x[0],
+                x[1],
+                x[0]**2,
+                (x[0]**2)*x[1]
+            ])
+
+        if self.choice == 2:
+            lift_x = np.array([
+                x[0],
+                x[1],
+                x[0]**2,
+                x[1]**2,
+                x[0]*x[1]
+            ])
+
+        if self.choice == 3:
+            lift_x = np.array([
+                x[0],
+                x[1],
+                x[0]**2
+            ])
+
+        if self.choice == 4:
+            lift_x = np.array([
+                x[0]**2,
+                x[1]**2,
+                x[0]*x[1],
+                x[0]**3,
+                x[1]**3,
+                (x[0]**2)*x[1],
+                (x[1]**2)*x[0]
+            ])
+
+        if self.choice == 5:
+            lift_x = np.array([
+                np.sin(x[0]),
+                np.sin(x[1]),
+                np.cos(x[0]),
+                np.cos(x[1])
+            ])
+
+        if self.choice == 6:
+            lift_x = np.array([
+                x[0],
+                x[1],
+                np.exp(x[0]),
+                np.exp(x[1])
+            ])
+
+        #NOTE: since all of our systems have scalar input,
+        #we only need a single control observable
+
+        lift = np.append(lift_x, u[0]) 
+
+        return lift
+
+    def poly(self, x, u):
+        """
+        Basis made up of polynomials up to order n
+        (includes all cross terms too, at user's request)
+        """
+
+        # make polynomials
+        # note: here I use lambda functions and store them in a list
+        polys = []
+        for n in range(self.order):
+            polys.append(lambda a, n=n: a**(n+1))
+        
+        # evaluate polynomials at current data point
+        lift_x = np.zeros(2*self.order)
+        for m in range(self.order):
+            lift_x[2*m] = polys[m](x[0], m)
+            lift_x[1 + 2*m] = polys[m](x[1], m)
+
+        # compute and evaluate cross terms, if desired
+        if self.cross_terms:
+            iter = 0
+            lift_cross_terms = np.zeros(self.order**2)
+            for j in range(self.order):
+                for i in range(self.order):
+                    lift_cross_terms[iter] = polys[j](x[0], j)*polys[i](x[1], i)
+                    iter += 1
+            lift_x = np.insert(lift_cross_terms, 0, lift_x)
+
+        #append with control observable
+        lift = np.append(lift_x, u[0])
+        return lift
+    
+    # The next two functions are helper functions for defining
+    # and evaluating Hermitian polynomials
+    def higher_derivative(self, func, order):
+        """
+        Compute higher order derivatives with autograd
+        """
+        for i in range(order):
+            func = grad(func)
+        return func
+    
+    def make_hermite(self, x, n):
+        """
+        Construct the Hermitian polynomial of order n
+        """
+        deriv_func = lambda v: np.exp(-0.5*v**2)
+        deriv_term = self.higher_derivative(deriv_func, n)
+        herm_poly = lambda a: (-1.)**n * np.exp(0.5*a**2) * deriv_term(a)
+        return np.array([herm_poly(x[0]), herm_poly(x[1])])
+
+    def hermite(self, x, u):
+        """
+        Basis made up of Hermitian polynomials up to order n
+        (includes all cross terms too, at user's request)
+
+        Note: refer to the markdown box below for the form
+        of an nth order Hermitian polynomial
+        """
+        # evaluate polynomials at current point
+        lift_x = np.zeros(2*self.order)
+        for m in range(self.order):
+            [lift_x1, lift_x2] = self.make_hermite(x, m)
+            lift_x[2*m] = lift_x1
+            lift_x[1 + 2*m] = lift_x2
+
+        # compute and evaluate cross terms, if desired
+        if self.cross_terms:
+            iter = 0
+            lift_cross_terms = np.zeros(self.order**2)
+            for j in range(self.order):
+                for i in range(self.order):
+                    lift_cross_terms[iter] = self.make_hermite(x, j)[0]*self.make_hermite(x, i)[1]
+                    iter += 1
+            lift_x = np.insert(lift_cross_terms, 0, lift_x)
+
+        #append with control observable
+        lift = np.append(lift_x, u[0])
+        return lift
---
+++ dynamic_models.py
@@ -0,0 +1,163 @@
+"""
+Describe this program
+
+IMPORTANT: right now these are all control systems
+TODO: give option for either controlled or unforced systems
+"""
+
+import autograd.numpy as np
+
+# Classes for each specific model
+class VanDerPol(object):
+    """
+    2D dynamics describing the van der pol osciallator
+    """
+    def __init__(self, eps=1.0, dt=0.001):
+        self.name = "VanDerPol"
+        self.eps = eps 
+        self.dt = dt
+        self.num_states = 2
+        self.num_inputs = 1
+        self.reset()
+
+    def reset(self, state=None):
+        if state is None:
+            self.state = np.random.uniform(-3., 3., size=(self.num_states,))
+        else:
+            self.state = state.copy()
+        return self.state.copy()
+    
+    def sample_input(self):
+        return np.random.uniform(-3., 3., size=(self.num_inputs,))
+
+    def dynamics(self, x, u):
+        xdot = np.array([
+            x[1],
+            -x[0] + self.eps * (1 - x[0]**2) * x[1] + u[0]
+        ])
+        return xdot
+
+    def step(self, u):
+        k1 = self.dt * self.dynamics(self.state, u)
+        k2 = self.dt * self.dynamics(self.state + k1/2., u)
+        k3 = self.dt * self.dynamics(self.state + k2/2., u)
+        k4 = self.dt * self.dynamics(self.state + k3, u)
+
+        self.state = self.state + (1/6.) * (k1+2.0*k2+2.0*k3+k4)
+        return self.state.copy()
+
+class ContinuousLinearSystem(object):
+    """
+    2D diagonal, linear dynamics (easy easy easy)
+    """
+    def __init__(self, dt=0.001):
+        self.name = "LinearSystem"
+        self.dt = dt
+        self.num_states = 2
+        self.num_inputs = 1
+        self.reset()
+        self.evals = [np.exp(-1*dt), np.exp(-2*dt)] # eigenvalues of the discretized system
+
+    def reset(self, state=None):
+        if state is None:
+            self.state = np.random.uniform(-10., 10., size=(self.num_states,))
+        else:
+            self.state = state.copy()
+        return self.state.copy()
+    
+    def sample_input(self):
+        return np.random.uniform(-1., 1., size=(self.num_inputs,))
+
+    def dynamics(self, x, u):
+        xdot = np.array([
+            x[0] + x[1],
+            -2*x[1] + u[0]
+        ])
+        return xdot
+
+    def step(self, u):
+        k1 = self.dt * self.dynamics(self.state, u)
+        k2 = self.dt * self.dynamics(self.state + k1/2., u)
+        k3 = self.dt * self.dynamics(self.state + k2/2., u)
+        k4 = self.dt * self.dynamics(self.state + k3, u)
+
+        self.state = self.state + (1/6.) * (k1+2.0*k2+2.0*k3+k4)
+        return self.state.copy()
+    
+class DiscreteLinearSystem(object):
+    """
+    discrete 2D dynamics (the simplest system of all time)
+    """
+    def __init__(self, dt=0.001): 
+        self.name = "LinearSystem"
+        self.dt = dt
+        self.num_states = 2
+        self.num_inputs = 1
+        self.reset()
+        self.evals = [0.5, -0.75]
+
+    def reset(self, state=None):
+        if state is None:
+            self.state = np.random.uniform(-5., 5., size=(self.num_states,))
+        else:
+            self.state = state.copy()
+        return self.state.copy()
+    
+    def sample_input(self):
+        return np.random.uniform(-1., 1., size=(self.num_inputs,))
+
+    def dynamics(self, x, u):
+        x_new = np.array([
+            self.evals[0]*x[0] + u[0],
+            self.evals[1]*x[1]
+        ])
+        return x_new
+
+    def step(self, u):
+        self.state = self.dynamics(self.state, u)
+        return self.state.copy()
+    
+class SystemWithKoopmanInvariantSubspace(object):
+    """
+    continuous 2D system which admits a finite-dimensional Koopman invariant subspace
+
+    Taken from "Koopman Invariant Subspaces and Finite Linear Representations of
+    Nonlinear Dynamical Systems for Control" (Steve) Brunton, (Bing) Brunton, Proctor, Kutz
+    """
+    def __init__(self, mu=-1., lamb=0.1, dt=0.001): 
+        self.name = "notlinear"
+        self.dt = dt
+        self.num_states = 2
+        self.num_inputs = 1
+
+        # note that we the default choices of mu and lambda
+        # the system is unstable (intentionally)
+        self.mu = mu
+        self.lamb = lamb
+        self.reset()
+
+    def reset(self, state=None):
+        if state is None:
+            self.state = np.random.uniform(-5., 5., size=(self.num_states,))
+        else:
+            self.state = state.copy()
+        return self.state.copy()
+
+    def sample_input(self):
+        return np.random.uniform(-1., 1., size=(self.num_inputs,))
+
+    def dynamics(self, x, u):
+        x_new = np.array([
+            self.mu*x[0],
+            self.lamb*(x[1] - x[0]**2) + u[0]
+        ])
+        return x_new
+
+    def step(self, u):
+        k1 = self.dt * self.dynamics(self.state, u)
+        k2 = self.dt * self.dynamics(self.state + k1/2., u)
+        k3 = self.dt * self.dynamics(self.state + k2/2., u)
+        k4 = self.dt * self.dynamics(self.state + k3, u)
+
+        self.state = self.state + (1/6.) * (k1+2.0*k2+2.0*k3+k4)
+        return self.state.copy()
